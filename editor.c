a1ab22377c30a14b4612331a0fba4c754801e11b
Shrey <shrma.shray@gmail.com>
2022-08-15 14:38:47 +0530

    2:38, 15/8/2022

--- editor.c
+++ editor.c
@@ -10,7 +10,7 @@
 
 //defines
 #define CTRL_KEY(k) ((k) & 0x1f)
-
+#define HEX_EDITOR_VERSION "0.0.1"
 //data
 
 struct editorConfig {
@@ -111,7 +111,22 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab,"~",1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "HEX EDITOR -- version %s", HEX_EDITOR_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab,"\r\n",2);
   }
@@ -120,12 +135,14 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
-  abAppend(&ab,"\x1b[2J",4);
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  
   abAppend(&ab,"\x1b[H",3);
   
   editorDrawRows(&ab);
   abAppend(&ab,"\x1b[H",3);
-
+ abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
